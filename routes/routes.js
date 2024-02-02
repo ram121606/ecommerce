f172c41ac4c4d86615cd9e722a913bb22e128741
sugavanaesh <sugisubbu123@gmail.com>
2024-02-02 10:46:06 +0530

    added get api

--- routes/routes.js
+++ routes/routes.js
@@ -5,26 +5,58 @@ import model1 from "../models/accessories.js";
 // mongoose.connect("mongodb+srv://sugisubbu123:navaneet@cluster0.uphzqod.mongodb.net/?retryWrites=true&w=majority");
 const routerA = express.Router();
 
-routerA.post("/insert",async (req,res)=>{
-    const {name,brand,size,rating,dod,stock,price,img} = req.body;
-    // console.log(req.body);
-    const response= await model.create({name, brand,size,rating,dod,stock,price,img})
-    // console.log(response);
-    res.send("success")
-})
-routerA.post('/register',(req,res)=>{
-    const {email, password} = req.body
+routerA.post("/insertcloth", async (req, res) => {
+  const { name, brand, size, rating, dod, stock, price, img } = req.body;
+  // console.log(req.body);
+  const response = await model.create({
+    name,
+    brand,
+    size,
+    rating,
+    dod,
+    stock,
+    price,
+    img,
+  });
+  // console.log(response);
+  res.send("success");
+});
 
-})
+routerA.post("/insertacc", async (req, res) => {
+  const { name, brand, size, rating, dod, stock, price, img } = req.body;
+  // console.log(req.body);
+  const response = await model1.create({
+    name,
+    brand,
+    size,
+    rating,
+    dod,
+    stock,
+    price,
+    img,
+  });
+  // console.log(response);
+  res.send("success");
+});
 
-routerA.post('/login',(req,res)=>{
-    const {email, password} = req.body
+// routerA.get("/products", (req,res)=>{
+//     const keyword = req.body.
+// })
 
-})
+routerA.get("/clothing", (req, res) => {
+  model.find({}).then((data)=>{res.send(data)}).catch((err)=>{console.log(err);})
+});
 
-routerA.post('/filter',(req,res)=>{
-    const {data} = req.body
-})
+routerA.get("/accessories", (req, res) => {
+    model1.find({}).then((data)=>{res.send(data)}).catch((err)=>{console.log(err);})
+  });
 
+routerA.post("/register", (req, res) => {
+  const { email, password } = req.body;
+});
 
-export { routerA }; 
+routerA.post("/login", (req, res) => {
+  const { email, password } = req.body;
+});
+
+export { routerA };
--- server.js
+++ server.js
@@ -9,18 +9,18 @@ import { acc } from './routes/accessories.js'
 
 import dotenv from 'dotenv'
 dotenv.config()
-// const db = process.env.URI
+const db = process.env.MONGO_URI
 // const port=process.env.PORT
 
 const app=express()
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 app.use(express.json())
 app.use('/auth',router)
 app.use("/",routerA)
 app.use("/cloth",cloth)
 app.use("/acc",acc)
 
-mongoose.connect("mongodb+srv://sugisubbu123:navaneet@cluster0.uphzqod.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect(db).then(()=>{console.log("<s>");}).catch((err)=>{console.log(err);})
 app.listen(3000,()=>{
     console.log(`connected on 3000`);
 })
